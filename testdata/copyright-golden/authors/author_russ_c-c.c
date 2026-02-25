f9bdf934b77d7690c3432d38b0d336729e439a21
Maxim Stykow <maxim.stykow@gmail.com>
2026-02-25 15:41:41 +0100

    test: refresh golden fixtures and ecosystem expected outputs

---
+++ testdata/copyright-golden/authors/author_russ_c-c.c
@@ -0,0 +1,9 @@
+/* common.c
+ *
+ * Functions for debugging and logging as well as some other
+ * simple helper functions.
+ *
+ * Russ Dill <Russ.Dill@asu.edu> 2001-2003
+ * Rewrited by Vladimir Oleynik <dzo@simtreas.ru> (C) 2003
+ *
+ */
