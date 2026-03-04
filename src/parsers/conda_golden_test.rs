dbabb8c6c3dab78303c90e5bc9f472e68d8b5285
Maxim Stykow <maxim.stykow@gmail.com>
2026-03-04 18:15:23 +0100

    fix(parsers): restore conda and opam golden parity

--- src/parsers/conda_golden_test.rs
+++ src/parsers/conda_golden_test.rs
@@ -2,8 +2,7 @@
 mod golden_tests {
     use super::super::PackageParser;
     use super::super::conda::{CondaEnvironmentYmlParser, CondaMetaYamlParser};
-
-    use std::fs;
+    use crate::test_utils::compare_package_data_parser_only;
     use std::path::PathBuf;
 
     /// Helper function to run golden tests.
@@ -18,28 +17,13 @@ mod golden_tests {
             _ => panic!("Unknown parser type: {}", parser_type),
         };
 
-        let expected_json = fs::read_to_string(expected_file)
-            .unwrap_or_else(|_| panic!("Failed to read expected file: {}", expected_file));
-
-        let actual_json =
-            serde_json::to_string_pretty(&package_data).expect("Failed to serialize PackageData");
-
-        let expected_value: serde_json::Value =
-            serde_json::from_str(&expected_json).expect("Failed to parse expected JSON");
-        let actual_value: serde_json::Value =
-            serde_json::from_str(&actual_json).expect("Failed to parse actual JSON");
-
-        if expected_value != actual_value {
-            println!("\n=== EXPECTED ===");
-            println!("{}", serde_json::to_string_pretty(&expected_value).unwrap());
-            println!("\n=== ACTUAL ===");
-            println!("{}", serde_json::to_string_pretty(&actual_value).unwrap());
-            panic!("Golden test failed: output does not match expected");
+        match compare_package_data_parser_only(&package_data, &PathBuf::from(expected_file)) {
+            Ok(()) => {}
+            Err(e) => panic!("Golden test failed for {}: {}", test_file, e),
         }
     }
 
     #[test]
-    #[ignore] // TODO: Verify expected output matches Rust implementation
     fn test_golden_meta_yaml_abeona() {
         run_golden(
             "testdata/conda/meta-yaml/abeona/meta.yaml",
@@ -49,7 +33,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_meta_yaml_gcnvkernel() {
         run_golden(
             "testdata/conda/meta-yaml/gcnvkernel/meta.yaml",
@@ -59,7 +42,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_meta_yaml_pippy() {
         run_golden(
             "testdata/conda/meta-yaml/pippy/meta.yaml",
@@ -69,7 +51,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_environment_ringer() {
         run_golden(
             "testdata/conda/conda-yaml/ringer/environment.yaml",
@@ -79,7 +60,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_environment_test() {
         run_golden(
             "testdata/conda/conda-yaml/test/environment_host_port.yml",
@@ -89,7 +69,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_environment_phc_gnn() {
         run_golden(
             "testdata/conda/conda-yaml/phc-gnn/environment_gpu.yml",
--- src/parsers/opam_golden_test.rs
+++ src/parsers/opam_golden_test.rs
@@ -2,8 +2,7 @@
 mod golden_tests {
     use super::super::PackageParser;
     use super::super::opam::OpamParser;
-
-    use std::fs;
+    use crate::test_utils::compare_package_data_parser_only;
     use std::path::PathBuf;
 
     /// Helper function to run golden tests.
@@ -13,37 +12,13 @@ mod golden_tests {
         let test_path = PathBuf::from(test_file);
         let package_data = OpamParser::extract_first_package(&test_path);
 
-        let expected_json = fs::read_to_string(expected_file)
-            .unwrap_or_else(|_| panic!("Failed to read expected file: {}", expected_file));
-
-        let actual_json =
-            serde_json::to_string_pretty(&package_data).expect("Failed to serialize PackageData");
-
-        let expected_value: serde_json::Value =
-            serde_json::from_str(&expected_json).expect("Failed to parse expected JSON");
-        let actual_value: serde_json::Value =
-            serde_json::from_str(&actual_json).expect("Failed to parse actual JSON");
-
-        // For OPAM expected files, we need to extract the first element of the array
-        let expected_pkg = if let Some(arr) = expected_value.as_array() {
-            arr.first()
-                .expect("Expected array should have at least one element")
-                .clone()
-        } else {
-            expected_value
-        };
-
-        if expected_pkg != actual_value {
-            println!("\n=== EXPECTED ===");
-            println!("{}", serde_json::to_string_pretty(&expected_pkg).unwrap());
-            println!("\n=== ACTUAL ===");
-            println!("{}", serde_json::to_string_pretty(&actual_value).unwrap());
-            panic!("Golden test failed: output does not match expected");
+        match compare_package_data_parser_only(&package_data, &PathBuf::from(expected_file)) {
+            Ok(()) => {}
+            Err(e) => panic!("Golden test failed for {}: {}", test_file, e),
         }
     }
 
     #[test]
-    #[ignore] // TODO: Verify expected output matches Rust implementation
     fn test_golden_sample1() {
         run_golden(
             "testdata/opam/sample1/sample1.opam",
@@ -52,7 +27,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample2() {
         run_golden(
             "testdata/opam/sample2/sample2.opam",
@@ -61,7 +35,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample3() {
         run_golden(
             "testdata/opam/sample3/sample3.opam",
@@ -70,25 +43,22 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample4() {
         run_golden(
-            "testdata/opam/sample4/sample4.opam",
+            "testdata/opam/sample4/opam",
             "testdata/opam/sample4/output.opam.expected",
         );
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample5() {
         run_golden(
-            "testdata/opam/sample5/sample5.opam",
+            "testdata/opam/sample5/opam",
             "testdata/opam/sample5/output.opam.expected",
         );
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample6() {
         run_golden(
             "testdata/opam/sample6/sample6.opam",
@@ -97,7 +67,6 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample7() {
         run_golden(
             "testdata/opam/sample7/sample7.opam",
@@ -106,10 +75,9 @@ mod golden_tests {
     }
 
     #[test]
-    #[ignore]
     fn test_golden_sample8() {
         run_golden(
-            "testdata/opam/sample8/sample8.opam",
+            "testdata/opam/sample8/opam",
             "testdata/opam/sample8/output.opam.expected",
         );
     }
