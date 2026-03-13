f76f3db7a095c28abe62659fd710d6017937d262
Maxim Stykow <maxim.stykow@gmail.com>
2026-03-13 11:43:03 +0100

    test(python): add dependency parser goldens

--- src/parsers/python_golden_test.rs
+++ src/parsers/python_golden_test.rs
@@ -56,4 +56,36 @@ mod golden_tests {
             Err(e) => panic!("Golden test failed: {}", e),
         }
     }
+
+    #[test]
+    fn test_golden_anonapi_wheel_metadata() {
+        let test_file = PathBuf::from(
+            "reference/scancode-toolkit/tests/packagedcode/data/pypi/unpacked_wheel/metadata-2.1/with_sources/anonapi-0.0.19.dist-info/METADATA",
+        );
+        let expected_file =
+            PathBuf::from("testdata/python/golden/anonapi-wheel-metadata-expected.json");
+
+        let package_data = PythonParser::extract_first_package(&test_file);
+
+        match compare_package_data_parser_only(&package_data, &expected_file) {
+            Ok(_) => (),
+            Err(e) => panic!("Golden test failed: {}", e),
+        }
+    }
+
+    #[test]
+    fn test_golden_anonapi_sdist_pkginfo() {
+        let test_file = PathBuf::from(
+            "reference/scancode-toolkit/tests/packagedcode/data/pypi/unpacked_sdist/metadata-1.2/anonapi-0.0.19/PKG-INFO",
+        );
+        let expected_file =
+            PathBuf::from("testdata/python/golden/anonapi-sdist-pkginfo-expected.json");
+
+        let package_data = PythonParser::extract_first_package(&test_file);
+
+        match compare_package_data_parser_only(&package_data, &expected_file) {
+            Ok(_) => (),
+            Err(e) => panic!("Golden test failed: {}", e),
+        }
+    }
 }
