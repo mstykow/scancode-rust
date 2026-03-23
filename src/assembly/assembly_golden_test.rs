b0237c215234c39fbdaae2789c3d45bffd793fbb
Maxim Stykow <maxim.stykow@gmail.com>
2026-03-23 10:18:45 +0100

    feat: add facet-based summary tallies

--- src/assembly/assembly_golden_test.rs
+++ src/assembly/assembly_golden_test.rs
@@ -135,6 +135,7 @@ mod tests {
                 is_readme: false,
                 is_top_level: false,
                 is_key_file: false,
+                facets: vec![],
                 tallies: None,
             };
 
